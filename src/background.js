aa86840529258052c9666cf2bfbd3309ff71f372
Brent Farris <brentfarris@outlook.com>
2022-08-04 22:01:01 -0500

    Removing tabs perms & reduce code

--- src/background.js
+++ src/background.js
@@ -3,19 +3,15 @@ function remove_context_box() {
 		"ytd-clarification-renderer",
 		"ytd-info-panel-container-renderer"
 	];
-	let cb = document.getElementById("clarify-box");
-	if (cb)
-		cb.remove();
 	for (let i = 0; i < names.length; ++i) {
 		let all = document.getElementsByTagName(names[i]);
-		if (all)
-			for (let j = 0; j < all.length; ++j)
-				all[j].remove();
+		for (let j = 0; all && j < all.length; ++j)
+			all[j].remove();
 	}
 }
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-	if (tab.url.indexOf("youtube.com/") >= 0) {
+	if (tab && tab.url && tab.url.indexOf("youtube.com/") >= 0) {
 		chrome.scripting.executeScript({
 			target: { tabId: tab.id },
 			function: remove_context_box
